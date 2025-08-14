aba6c4eabe7e274bce98080ee0ad154f3aba69c1
Starina-Lokich <starinalokich@gmail.com>
2025-08-14 15:30:18 +0300

    feat: добавить начальные конфиги проекта и CI/CD
    - добавить package.json с зависимостями
    - добавить webpack.config.js для сборки проекта
    - добавить yarn.lock для управления зависимостями
    - добавить workflows/deploy.yml для автоматического деплоя через GitHub Actions

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    open: true,
+  },
+  mode: 'production',
+};
